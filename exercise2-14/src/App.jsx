42a6fc076f69b1391d8c64b52c003ebbf01aaf16
Student <student@sirafrica.org>
2026-03-12 12:49:26 +0300

    name and number can be deleted successful with conformation

---
+++ exercise2-14/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import personServices from './services/persons'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter,setFilter] = useState('')
+
+  useEffect(() => {
+    personServices.getAll().then(response => {
+      setPersons(response)
+    })
+  },[])
+  
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+  const nameExists = persons.some(person => person.name === newName)
+
+  if (nameExists) {
+    alert(`${newName} is already added to phonebook`)
+    return
+  }
+
+    const newNameObject = {
+      name: newName,
+      number:newNumber,
+      id: persons.length + 1
+    }
+
+    personServices.create( newNameObject).then(response => {
+      setPersons(persons.concat(response))
+    })
+    
+    console.log('new person added', newNameObject)
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const deleteAction = (id) => {
+    const numberId = persons.find(p => p.id===id)
+     if(window.confirm(`Delete name: ${numberId.name} and number: ${numberId.number} `)) {
+      personServices.remove(id).then(response => {
+        setPersons(persons.filter(p => p.id !==id))
+      })
+     }
+    console.log('number ' + id + ' needs to be deleted')
+  }
+
+  // Filtered persons based on the filter input (case-insensitive)
+  const filterData = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter = {filter} setFilter={setFilter}  />
+
+        <h1><p>Add a new</p></h1>
+       <PersonForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        addNewPerson={addNewPerson}
+       />
+      <h2>Numbers</h2>
+       <Persons
+        filterData={filterData}
+        deleteAction={deleteAction}
+       />
+    </div>
+  )
+}
+
+export default App
