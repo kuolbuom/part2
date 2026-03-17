53be332bba9fa5cd32411d35f251ad1ebeda9796
kuol buom <kuolbuom111@gmail.com>
2026-03-17 14:10:28 +0300

    improved error for unsuccessful message

---
+++ phonebook/exercise2-17/src/services/persons.js
@@ -0,0 +1,25 @@
+import axios from 'axios'; 
+
+const baseUrl = 'http://localhost:3003/persons';
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = (newNameObject) => {
+    const request = axios.post(baseUrl, newNameObject)
+    return request.then( response => response.data)
+}
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const update = (id, changePerson) => {
+    const request = axios.put(`${baseUrl}/${id}`, changePerson)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, remove, update}
