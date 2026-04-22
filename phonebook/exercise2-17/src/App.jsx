93f7d3723f30dd34e08cfeff99409f26cdc9559c
kuol buom <kuolbuom111@gmail.com>
2026-04-22 17:13:25 +0300

    error validation fixed

--- phonebook/exercise2-17/src/App.jsx
+++ phonebook/exercise2-17/src/App.jsx
@@ -10,88 +10,91 @@ import Notification4 from './components/Notification4'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filter,setFilter] = useState('')
+  const [filter, setFilter] = useState('')
   const [message, setMessage] = useState(null)
   const [replaceMessage, setReplaceMessage] = useState(null)
   const [showMessage, setShowMessage] = useState(null)
   const [validationError, setValidationError] = useState(null)
-  
+
 
   useEffect(() => {
     personServices.getAll().then(response => {
       setPersons(response)
     })
-  },[])
-  
+  }, [])
+
 
   const addNewPerson = (event) => {
-  event.preventDefault()
-  // finding and shecking for existance base on it name
-  const existingPerson = persons.find(person => person.name === newName)
+    event.preventDefault()
+    // finding and shecking for existance base on it name
+    const existingPerson = persons.find(person => person.name === newName)
 
-  if (existingPerson) {
-     //shallow copy
-    const changePerson = { ...existingPerson, number: newNumber}
-     //confirmation and delete
-    if (window.confirm(`${newName} is already added. Replace the old number?`)) {
+    if (existingPerson) {
+      //shallow copy
+      const changePerson = { ...existingPerson, number: newNumber }
+      //confirmation and delete
+      if (window.confirm(`${newName} is already added. Replace the old number?`)) {
+        personServices
+          .update(existingPerson.id, changePerson)
+          .then(response => {
+            setPersons(persons.map(person =>
+              person.id !== existingPerson.id ? person : response
+            ))
+            setReplaceMessage(`${newName}'s number replaced successfully`)
+            setTimeout(() => {
+              setReplaceMessage(null)
+            }, 10000);
+          }).catch(error => {
+            setShowMessage(`information of ${newName} has already been removed from server`)
+            setTimeout(() => {
+              setShowMessage(null)
+            }, 10000);
+          })
+      }
+
+    } else {
+      // the rest stay as its
+      const newNameObject = {
+        name: newName,
+        number: newNumber
+      }
+
       personServices
-        .update(existingPerson.id, changePerson)
+        .create(newNameObject)
         .then(response => {
-          setPersons(persons.map(person =>
-            person.id !== existingPerson.id ? person : response
-          ))
-          setReplaceMessage(`${newName}'s number replaced successfully`)
+          setPersons(persons.concat(response))
+          setMessage(`added '${newName}' sucessfully`)
           setTimeout(() => {
-            setReplaceMessage(null)
-          }, 10000);
-        }).catch(error => {
-        setShowMessage(`information of ${newName} has already been removed from server`)
-        setTimeout(() => {
-          setShowMessage(null)
-        }, 10000);
-      })
+            setMessage(null)
+          }, 10000)
+        })
+        .catch(error => {
+          setValidationError(error.response.data.error)
+          setTimeout(() => {
+            setValidationError(null)
+          }, 10000)
+          console.log(error.response)
+          console.log(error.response.data)
+          console.log(error.response.data.error)
+
+        })
     }
 
-  } else {
-    // the rest stay as its
-    const newNameObject = {
-      name: newName,
-      number: newNumber
-    }
-
-    personServices
-      .create(newNameObject)
-      .then(response => {
-        setPersons(persons.concat(response))
-        setMessage(`added '${newName}' sucessfully`)
-          setTimeout(() => {
-          setMessage(null)
-        }, 10000)
-      })
-      .catch(error => {
-        setValidationError(`Person validation failed: name: Path  'name'('${newName}', length 2) is shorter than the minimum allowed length (3).`)
-        setTimeout(() =>{
-          setValidationError(null)
-        },10000)
-        console.log('validation Error',error.response.data.error)
-      })
+    setNewName('')
+    setNewNumber('')
   }
 
-  setNewName('')
-  setNewNumber('')
-}
-
   const deleteAction = (id) => {
-    const numberId = persons.find(p => p.id===id)
-     if(window.confirm(`Delete name: ${numberId.name} and number: ${numberId.number} `)) {
+    const numberId = persons.find(p => p.id === id)
+    if (window.confirm(`Delete name: ${numberId.name} and number: ${numberId.number} `)) {
       personServices.remove(id).then(response => {
-        setPersons(persons.filter(p => p.id !==id))
-        
+        setPersons(persons.filter(p => p.id !== id))
+
       })
-     }
+    }
     console.log('number ' + id + ' needs to be deleted')
   }
 
@@ -106,22 +109,22 @@ const App = () => {
       <Notification message={message} />
       <Notification2 replaceMessage={replaceMessage} />
       <Notification3 showMessage={showMessage} />
-      <Notification4 validationError={validationError}/>
-      <Filter filter = {filter} setFilter={setFilter}  />
+      <Notification4 validationError={validationError} />
+      <Filter filter={filter} setFilter={setFilter} />
 
-        <h1><p>Add a new</p></h1>
-       <PersonForm
+      <h1><p>Add a new</p></h1>
+      <PersonForm
         newName={newName}
         setNewName={setNewName}
         newNumber={newNumber}
         setNewNumber={setNewNumber}
         addNewPerson={addNewPerson}
-       />
+      />
       <h2>Numbers</h2>
-       <Persons
+      <Persons
         filterData={filterData}
         deleteAction={deleteAction}
-       />
+      />
     </div>
   )
 }
